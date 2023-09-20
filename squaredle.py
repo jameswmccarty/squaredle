82196fd47b5083928666d44c7eca9891e1038dde
jameswmccarty <jameswmccarty@gmail.com>
2023-09-20 13:04:06 -0400

    Add files via upload

--- squaredle.py
+++ squaredle.py
@@ -28,9 +28,7 @@ class Node:
 			self.children[value[0]].insert(value[1:])
 
 	def test(self,char):
-		if char in self.children.keys():
-			return True
-		return False
+		return char in self.children.keys()
 
 	def walk(self,char):
 		if self.test(char):
@@ -55,16 +53,19 @@ def words_from_pos(index,dims,letters,prefix_map):
 			found.add((built,moves[1:]))
 		for dx,dy in move_list.keys():
 			nx,ny = x+dx,y+dy
-			if nx >= 0 and nx < dims[0] and ny >= 0 and ny < dims[1] and (nx,ny) not in seen and location.test(letters[ny*dims[0]+nx]):
-				next_seen = seen.copy()
-				next_seen.add((nx,ny))
-				q.append(((nx,ny),built+letters[ny*dims[0]+nx],moves+','+move_list[(dx,dy)],next_seen,location.walk(letters[ny*dims[0]+nx])))
+			if nx >= 0 and nx < dims[0] and ny >= 0 and ny < dims[1]:
+				next_letter = letters[ny*dims[0]+nx]
+				if (nx,ny) not in seen and location.test(next_letter):
+					next_seen = seen.copy()
+					next_seen.add((nx,ny))
+					q.append(((nx,ny),built+next_letter,moves+','+move_list[(dx,dy)],next_seen,location.walk(next_letter)))
 	return sorted(found)
 
 def solve(puzzle,dims,prefix_map):
-	for idx in range(len(puzzle)):
-		print(f"{idx} : ({idx%dims[0]},{idx//dims[0]}) {puzzle[idx]}")
-		print(words_from_pos(idx,dims,puzzle,prefix_map))
+	for idx,char in enumerate(puzzle):
+		if prefix_map.test(char):
+			print(f"{idx} : ({idx%dims[0]},{idx//dims[0]}) {puzzle[idx]}")
+			print(words_from_pos(idx,dims,puzzle.upper(),prefix_map.walk(char)))
 
 if __name__ == "__main__":
 
@@ -73,6 +74,8 @@ if __name__ == "__main__":
 	
 	root = build_prefix_map(word_list)
 
-	solve("CDHEEYOTENTFANRA",(4,4),root)
+	#solve("CDHEEYOTENTFANRA",(4,4),root)
 
-	#solve("VRLTOGANIKSGMLAEUOLBRDWPF",(5,5),root)
+	solve("VRLTOGANIKSGMLAEUOLBRDWPF",(5,5),root)
+
+
